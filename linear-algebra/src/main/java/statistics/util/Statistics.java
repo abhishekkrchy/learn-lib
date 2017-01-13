90578bfd2544740ba4c7deb9b9c6f54c14ca0f6e
abhishekkrchy <abhishek.kr.chy@gmail.com>
2017-01-13 19:36:07 +0530

    removed old method

--- linear-algebra/src/main/java/statistics/util/Statistics.java
+++ linear-algebra/src/main/java/statistics/util/Statistics.java
@@ -9,16 +9,16 @@ import java.util.Random;
 public class Statistics {
     public static double meanSquaredError(DenseVector denseVector1, DenseVector denseVector2) {
         double total = 0.0;
-        for (int i = 0; i < denseVector1.getSize(); i++) {
+        for (int i = 0; i < denseVector1.size(); i++) {
             total += Math.pow(denseVector1.value(i) - denseVector2.value(i), 2.0);
         }
-        return total / denseVector1.getSize();
+        return total / denseVector1.size();
     }
 
     public static MarkedNode meanSquaredError(DenseVector denseVector1, DenseVector denseVector2, int varPos) {
         double total = 0.0;
         MarkedNode markedNode = new MarkedNode();
-        for (int i = 0; i < denseVector1.getSize(); i++) {
+        for (int i = 0; i < denseVector1.size(); i++) {
             if (i == varPos) {
                 MarkedNode temp = new MarkedNode(-1, denseVector1.value(i));
                 markedNode.setChildNode(temp);
@@ -26,9 +26,9 @@ public class Statistics {
                 total += Math.pow(denseVector1.value(i) - denseVector2.value(i), 2.0);
             }
         }
-        markedNode.setAdditiveConstant(total / denseVector1.getSize());
+        markedNode.setAdditiveConstant(total / denseVector1.size());
         markedNode.setChildNodeExponent(2.0);
-        markedNode.setChildNodeMultiplicand(1 / (denseVector1.getSize()));
+        markedNode.setChildNodeMultiplicand(1 / (denseVector1.size()));
         markedNode.setChildFunctionalRelation(AlgebraicFunction.ADD);
         return markedNode;
     }
